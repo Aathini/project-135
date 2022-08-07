8ae133090d187af8974b09cd13734c97927dd372
Aathini <78332561+Aathini@users.noreply.github.com>
2022-08-07 11:28:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Status = "";
+input_text = "";
+function setup() {
+    canvas = createCanvas(400,250);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400,250);
+    video.hide();
+}
 function start() {
-    
+    object = ml5.objectdetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects...";
+    input_text = getElementById("input_id").value;
+}
+function modelLoaded() {
+    console.log("Model has been Loaded");
+    Status = true;
+}
+function draw() {
+    image(video, 0, 0, 400, 250);
 }
